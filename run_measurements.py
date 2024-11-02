6c9c2bb37a01c0b21607ceb329529f8855f663ef
lyq12345 <yuqiaol5@uci.edu>
2024-11-02 11:40:03 -0700

    proc_time measurement template

--- run_measurements.py
+++ run_measurements.py
@@ -3,8 +3,11 @@ import time
 import argparse
 from split_model import get_model_config, initialize_blocks
 from torchstat import stat
+from flops_profiler.profiler import get_model_profile
+from thop import profile, clever_format
+import pandas as pd
 
-def load_blocks(model_type='resnet50', path='models/cifar10', device='cpu'):
+def load_blocks(model_type='resnet50', path='models/cifar10', device=torch.device("cpu")):
     """Load all blocks for the specified model type."""
     # Get model configuration
     ModelClass, block_configs, block_type = get_model_config(model_type)
@@ -32,6 +35,11 @@ def measure_block_times(blocks, x, device):
     times = {}
     
     with torch.no_grad():
+        # Dummy execution
+        start = time.time()
+        out1, _ = blocks['block1'](x)
+        times['dummy'] = time.time() - start
+
         # Block 1
         start = time.time()
         out1, _ = blocks['block1'](x)
@@ -40,19 +48,37 @@ def measure_block_times(blocks, x, device):
         # Block 2
         start = time.time()
         out2, _ = blocks['block2'](out1)
+        print(out1.shape)
         times['block2'] = time.time() - start
         
         # Block 3
         start = time.time()
         out3, _ = blocks['block3'](out2)
+        print(out2.shape)
         times['block3'] = time.time() - start
         
         # Block 4
         start = time.time()
         _ = blocks['block4'](out3)
+        print(out3.shape)
         times['block4'] = time.time() - start
     
     return times
+
+def measrue_idle_voltage_and_currency(blocks, x):
+    pass
+
+def measure_flops(blocks, block_num, x, device):
+    flops = {}
+    input_sizes = [(1, 3, 32, 32), (1, 24, 8, 8), (1, 32, 4, 4), (1, 64, 2, 2)]
+    with torch.no_grad():
+        for i in range(block_num):
+            model = blocks[f"block{i+1}"]
+            mocked_input = torch.zeros(input_sizes[i]).to(device)
+            flops, params = profile(model.to(device), inputs=(mocked_input, ))
+            flops, params = clever_format([flops, params], '%.3f')
+            print(flops, params)
+
 
 def main():
     # Add command line arguments
@@ -63,6 +89,10 @@ def main():
                       default='cpu', help='Device to use for computation')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size') 
     parser.add_argument('--path', type=str, default='models/cifar10', help='Path to model blocks')
+    parser.add_argument('--host_device', type=str, choices=['rpi', 'nano'],
+                        default='rpi', help='Device that run the models')
+    parser.add_argument('--metrics', type=str, default='flops', choices=['flops', 'proc_time', 'VA'], nargs='+', help='the metrics to measure')
+    parser.add_argument('--block_num', type=int, default=4, help='the number of blocks to run')
 
     args = parser.parse_args()
     
@@ -71,14 +101,25 @@ def main():
     # Load blocks and create input
     print(f"\nMeasuring execution times for {args.model}")
     blocks, x = load_blocks(args.model, args.path, device)
-    
-    # Measure execution times
-    times = measure_block_times(blocks, x, device)
-    
-    # Print results
-    print("\nExecution times:")
-    for block_name, execution_time in times.items():
-        print(f"{block_name} execution time: {execution_time*1000:.2f} ms")
+
+    metrics = args.metrics
+    block_num = args.block_num
+
+    for metric in metrics:
+        if metric == 'proc_time':
+            #Measure execution times
+            times = measure_block_times(blocks, x, device)
+            # Print results
+            print("\nExecution times:")
+            for block_name, execution_time in times.items():
+                print(f"{block_name} execution time: {execution_time*1000:.2f} ms")
+        elif metric == 'flops':
+            # Measure flops
+            measure_flops(blocks, block_num, x, device)
+        elif metric == 'VA':
+            pass
+        else:
+            print(f"Unknown metric {metric}!")
 
 if __name__ == "__main__":
     main()
